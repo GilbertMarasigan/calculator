3231f6724539eb993249c40f1fd7061a400abaa0
Gilbert <21050167+GilbertMarasigan@users.noreply.github.com>
2024-11-28 17:43:18 +0800

    operator functions and fix delete and clear

--- calc.js
+++ calc.js
@@ -5,6 +5,7 @@ const previousOperand = document.querySelector('.previous-operand');
 
 let arrCurrentOperand = [];
 let currentNumberOperand = 0;
+let operatorCount = 0;
 
 // initialize calculator
 function initCalc() {
@@ -12,32 +13,113 @@ function initCalc() {
     previousOperand.textContent = "";
     currentNumberOperand = 0;
     arrCurrentOperand = [];
+    operatorCount = 0;
 }
 
 initCalc();
 
+function isNumber(value) {
+    return !isNaN(parseFloat(value)) && isFinite(value);
+}
+
+
+function operate() {
+
+    let num1 = [];
+    let num2 = [];
+    let operator = null;
+    let result = null;
+
+    arrCurrentOperand.forEach((item) => {
+
+        if (["+", "-", "×", "÷"].includes(item)) {
+            operator = item;
+        } else {
+            if (operator) {
+                num2.push(item);
+            } else {
+                num1.push(item);
+            }
+        }
+
+    });
+
+    num1 = Number(num1.join(''));
+    num2 = Number(num2.join(''));
+
+    console.log({
+        "num1": num1,
+        "num2": num2,
+        "operator": operator
+    });
+
+    // perform operation
+
+    switch (operator) {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "÷":
+            result = num1 / num2;
+            break;
+        case "×":
+            result = num1 * num2;
+            break;
+        default:
+            break;
+    }
+
+    console.log({ result });
+
+    // display on previous operation
+    previousOperand.textContent = `${num1} ${operator} ${num2}`;
+    currentNumberOperand = result;
+    currentOperand.textContent = `${result}`;
+    arrCurrentOperand = result.toString().split('');
+    console.log(arrCurrentOperand);
+
+}
+
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => {
+
     button.addEventListener("click", function (e) {
 
         console.log(`${this.className} clicked!`);
 
         // number
-        if (this.className == "number") {
+        if (this.className == "number" || this.className == "number zero") {
             arrCurrentOperand.push(this.textContent);
             currentNumberOperand = arrCurrentOperand.join('');
+            // currentOperand.textContent = currentNumberOperand;
         }
 
         // operator
+        else if (this.className == "operator") {
+            if (operatorCount < 1) {
+                arrCurrentOperand.push(this.textContent);
+                currentNumberOperand = arrCurrentOperand.join('');
+                // currentOperand.textContent = currentNumberOperand;
+                operatorCount++;
+            }
+            else {
+                alert("Can only perform one operation at a time");
+            }
+        }
 
         // delete
-        if (this.className == "delete") {
-           
-            arrCurrentOperand.pop();
+        else if (this.className == "delete") {
+
+            const lastElement = arrCurrentOperand.pop();
+
+            if (!isNumber(lastElement)) operatorCount = 0;
             currentNumberOperand = arrCurrentOperand.join('');
 
-            if(arrCurrentOperand.length == 0){
+            if (arrCurrentOperand.length == 0) {
                 currentNumberOperand = 0;
                 arrCurrentOperand = [];
                 initCalc();
@@ -47,16 +129,22 @@ buttons.forEach(button => {
         }
 
         // clear
-        if (this.className == "clear") {
+        else if (this.className == "clear") {
             initCalc();
         }
 
         // number zero
 
         // equals
+        else if (this.className == "equals") {
+            operate(arrCurrentOperand);
+            operatorCount = 0;
+        }
 
-        
-        // display event
+        else {
+
+        }
+
         currentOperand.textContent = currentNumberOperand;
 
     });
